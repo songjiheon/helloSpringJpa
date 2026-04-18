5f9916a3bc82384119a3bee4d0673ff6290c04f6
song jiheon <syyyh1008@gmail.com>
2026-04-18 17:22:06 +0900

    CategoryForm 추가(엔티티 변환 기능, 제약조건 추가)

---
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -0,0 +1,32 @@
+package kr.ac.hansung.cse.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryForm {
+
+    private Long id;
+
+    @NotBlank(message = "카테고리 이름을 입력하세요")
+    @Size(max=50, message = "50자 이내로 입력하세요")
+    private String name;
+
+    //Form에서 Entity로 변환
+    public Category toEntity() {
+        return new Category(this.name);
+    }
+
+    //Entity에서 Form으로 변환
+    public static CategoryForm from(Category category) {
+        CategoryForm form = new CategoryForm();
+        form.id = category.getId();
+        form.name = category.getName();
+        return form;
+    }
+}
